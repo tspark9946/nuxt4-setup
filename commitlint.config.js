e94047087565494ae774c68bd9075bd7951d376b
Park taeseong <tspark9946@gmail.com>
2025-10-24 21:00:54 +0900

    eslint last setting

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test']],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-  },
-};
